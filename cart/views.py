bf9becdff27f32287b7fbc12ab68f6b63f135eb7
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-15 18:12:11 +0000

    Add  products to  cart functionality
    Add the quantity for product, add the view, add the url, style the buttons on base.css

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 # Create your views here.
 
@@ -6,3 +8,22 @@ def view_cart(request):
     """ A view that renders the cart contents page """
 
     return render(request, 'cart/cart.html')
+
+def add_to_cart(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+    print(product)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
