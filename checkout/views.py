952975096c435e9c0d4061d0e3419e3b65247fcd
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-17 12:21:26 +0000

    Add stripe payment elements
    Add js file to store the elements, add stripe key to views.py, style the checkout form in checkout.css, add link to checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51OZX2wGCWPCfXvnmhLyH72DDLI6Hpp6UpeQsTh6NsCiGyfMqYAl8GDcllomyWLLXucbRIQRY0Ye5RrypIenISWOc00nX21sDeV',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
