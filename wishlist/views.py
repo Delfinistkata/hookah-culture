4d725feaf9495d55e02758105572e52ac470f7be
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-24 13:56:07 +0000

    Add views and template for wishlist

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from profiles.models import UserProfile
+from .models import Wishlist
+
+
+
+def wishlist(request):
+    """ A view to return the wishlist page """
+
+    user_profile = UserProfile.objects.get(user=request.user)
+    user_wishlist = Wishlist.objects.filter(user_profile=user_profile)
+
+    return render(
+        request, 'wishlist/wishlist.html', {'user_wishlist': user_wishlist}
+    )
