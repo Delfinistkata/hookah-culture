3b2650ef87a07aec523066584bc7d892e42daef5
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-24 15:02:47 +0000

    Add functionality for add products to wishlist
    Add the main layout for the wishlish, add the view for adding products, add the url, add the signals.py, add the contexts.py, add styling in base.css for the button to add products in the wishlist, add the button in the products_detail.html, add the contexts for wishlist in settings.py

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
+from products.models import Product
 from profiles.models import UserProfile
 from .models import Wishlist
 
 
 
+@login_required
 def wishlist(request):
     """ A view to return the wishlist page """
 
@@ -14,3 +18,31 @@ def wishlist(request):
     return render(
         request, 'wishlist/wishlist.html', {'user_wishlist': user_wishlist}
     )
+
+
+@login_required
+def add_to_wishlist(request, product_id):
+    """
+    Add a product to the user's wishlist.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    # Check if the product is already in the users wishlist
+    if Wishlist.objects.filter(
+        user_profile=user_profile,
+        product=product
+    ).exists():
+        messages.warning(
+            request, f'{product.name} is already in your Wishlist.')
+    else:
+        # create a new wishlist item
+        wishlist_item = Wishlist.objects.create(
+            user_profile=user_profile, product=product)
+        messages.success(
+            request,
+            f'{wishlist_item.product.name} added to Wishlist successfully!'
+        )
+
+    # Redirect to the product detail page
+    return redirect(reverse('product_detail', args=[product_id]))
