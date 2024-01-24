18e3471c040088dd13ec08e8fc3f0cd73f917c3b
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-24 17:39:33 +0000

    Add view for show_reviews, urls and templates
    Add urls to main urls.py, add the view for show_reviews, add reviews template with basic information, add basic style, add product reviews to product_detail.html

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Review
 
-# Create your views here.
+
+@login_required
+def show_reviews(request):
+    """ A view to show the user's product reviews """
+    reviews = Review.objects.filter(author=request.user)
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'reviews/reviews.html', context)
