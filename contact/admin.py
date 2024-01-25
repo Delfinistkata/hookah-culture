76500c502a530d629b8e582330479b53cefc43ae
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-25 12:00:46 +0000

    Add views, urls,templates and style
    Add links to the main header and mobile header to contact Us, add the form in the admin panel, add the urls in the main project and the url for the form, create a form and add the fields, add the views for the form, create basic html with google maps and add style to it

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    """
+    Contact management section for admin
+    """
+    list_display = (
+        "date_created",
+        "topic",
+        "name",
+        "email",
+        "message",
+        "status",
+    )
+
+    list_filter = ("date_created", "name", "topic", "status")
+
+    ordering = ("-date_created",)
+
+admin.site.register(Contact)
