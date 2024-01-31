5ac45c6ee454f658c560fe28ff7a3a8918692995
Delfinistkata <elidimitrovadkit@gmail.com>
2024-01-31 16:26:39 +0000

    Add stock logic update after buying

--- cart/views.py
+++ cart/views.py
@@ -12,20 +12,25 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping cart """
 
-    product = get_object_or_404(Product, pk=item_id)
-    print(product)
+    product = get_object_or_404(Product, pk=item_id)    
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    cart = request.session.get('cart', {})
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += quantity
-        messages.success(request, f'Added {product.name} to your cart')
+    # Check if user already submitted a review for the product
+    if quantity > product.stock :
+        messages.error(request,
+                        'Out of stock')
     else:
-        cart[item_id] = quantity
-        messages.success(request, f'Added {product.name} to your cart')
+        cart = request.session.get('cart', {})
 
-    request.session['cart'] = cart
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+            messages.success(request, f'Added {product.name} to your cart')
+        else:
+            cart[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
+
+        request.session['cart'] = cart
     return redirect(redirect_url)
 
 
