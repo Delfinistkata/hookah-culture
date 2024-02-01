1ed07b50f47dc1310f06460822e468cf966d2e00
Delfinistkata <elidimitrovadkit@gmail.com>
2024-02-01 13:07:14 +0000

    Remove secret key and set debug

--- hookah_culture/settings.py
+++ hookah_culture/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_q-sq*k%6y5f@xir&w)uiokntpfyo1ktrsx4lt0b6f=^=%kuf('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['hookah-culture-2bfc66e2f4dc.herokuapp.com', 'localhost']
 
