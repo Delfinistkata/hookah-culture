51ca5a9c6df2bc313df03e95042700f4451550d3
Delfinistkata <elidimitrovadkit@gmail.com>
2024-02-13 19:31:12 +0000

    Set debug to true

--- hookah_culture/settings.py
+++ hookah_culture/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['hookah-culture-2bfc66e2f4dc.herokuapp.com', '8000-delfinistka-hookahcultu-kj4dfkcro36.ws-eu107.gitpod.io', '8000-delfinistka-hookahcultu-kj4dfkcro36.ws-eu108.gitpod.io']
 
